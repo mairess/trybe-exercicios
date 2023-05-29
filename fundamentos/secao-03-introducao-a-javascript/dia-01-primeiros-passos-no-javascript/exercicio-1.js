94421cb5669d4c61d01457265e1c9517656864ca
Maires Souza <trabalho.maires@gmail.com>
2023-05-29 15:59:48 -0300

    add exercise 1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicio-1.js
@@ -0,0 +1,17 @@
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. 
+// É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. 
+// Escreva códigos para:
+// Adição (num1 + num2)
+// Subtração (num1 - num2)
+// Multiplicação (num1 * num2)
+// Divisão (num1 / num2)
+// Módulo (num1 % num2)
+
+let num1 = 6;
+let num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
