9572cb5fcedc769370674f55dcdd1130631cf526
Maires Souza <trabalho.maires@gmail.com>
2023-06-14 08:47:46 -0300

    feat: add requisite number 5

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
@@ -37,6 +37,13 @@ myWebpage.addEventListener('dblclick', () => {
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
+myWebpage.addEventListener('mouseover', () => {
+  myWebpage.style.background = 'green';
+});
+
+myWebpage.addEventListener('mouseout', () => {
+  myWebpage.style.background = 'unset';
+});
 
 // Segue abaixo um exemplo de uso do event.target.
 
