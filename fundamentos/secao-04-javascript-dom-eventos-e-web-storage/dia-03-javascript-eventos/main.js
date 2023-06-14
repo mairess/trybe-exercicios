47c14fea66ecceb45a2b11b53bf687b14ad3e3c9
Maires Souza <trabalho.maires@gmail.com>
2023-06-14 08:47:10 -0300

    feat: add requisite number 4.1

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
@@ -31,6 +31,9 @@ input.addEventListener('input', (event) => {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
+myWebpage.addEventListener('dblclick', () => {
+  window.location.replace('https://github.com/mairess/trybe-exercicios');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
