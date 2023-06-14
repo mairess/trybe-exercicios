a4d8a7e89e9ef2eef28e530ee98fbada6c49e0df
Maires Souza <trabalho.maires@gmail.com>
2023-06-14 08:56:19 -0300

    feat: add blank target to requisite number 4.1

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/main.js
@@ -32,7 +32,7 @@ input.addEventListener('input', (event) => {
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
 myWebpage.addEventListener('dblclick', () => {
-  window.location.replace('https://github.com/mairess/trybe-exercicios');
+  window.open('https://github.com/mairess/trybe-exercicios', '_blank');
 });
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
