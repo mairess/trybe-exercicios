b22cf1b7dc240413e9e2c3c2fe5b9d236a2ed630
Maires Souza <trabalho.maires@gmail.com>
2023-06-29 17:25:06 -0300

    update gitignore

--- fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercícios/removeItem.test.js
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercícios/removeItem.test.js
@@ -2,18 +2,18 @@ const removeItem = require('./removeItem.js');
 
 describe('Requisite 1', () => {
     test('check if the function call removeItem([1, 2, 3, 4], 3) return [1, 2, 4]', () => {
-        expect(removeItem([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+        expect(removeItem([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     });
 });
 
 describe('Requisite 2', () => {
     test('check if the function call removeItem([1, 2, 3, 4], 3) DO NOT return [1, 2, 3, 4]', () => {
-        expect(removeItem([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
+        expect(removeItem([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
     });
 });
 
 describe('Requisite 3', () => {
     test('check if the function call removeItem([1, 2, 3, 4], 5), return [1, 2, 3, 4]', () => {
-        expect(removeItem([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4])
+        expect(removeItem([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
     });
 });
