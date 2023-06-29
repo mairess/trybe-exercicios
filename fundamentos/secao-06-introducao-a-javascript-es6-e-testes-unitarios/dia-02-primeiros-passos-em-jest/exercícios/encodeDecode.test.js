db2143a54ed9ade22c69bd8a0e4a75cee14466fe
Maires Souza <trabalho.maires@gmail.com>
2023-06-29 19:06:07 -0300

    feat: unit testing encodeDecode function

--- fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercícios/encodeDecode.test.js
+++ fundamentos/secao-06-introducao-a-javascript-es6-e-testes-unitarios/dia-02-primeiros-passos-em-jest/exercícios/encodeDecode.test.js
@@ -1,11 +1,37 @@
 const { encode, decode } = require('./encodeDecode.js');
 
-describe('Test encode & decode are functions', () => {
+describe('Tests encode function', () => {
     test('if typeof encode is function', () =>{
         expect(typeof encode).toBe('function');
     });
 
+    test(`if encode vowels 'a, e, i, o, u' are converted to '1, 2, 3, 4, 5' respectivelly`, () => {
+        expect(encode('encode')).toBe('2nc4d2');
+    });
+
+    test(`if encode letters 'b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, z' ARE NOT converted to numbers`, () => {
+        expect(encode('bcdfghjklmnpqrstvwxz')).toBe('bcdfghjklmnpqrstvwxz');
+    });
+
+    test('Check if the function returns a string with same length passed as a parameter', () =>{
+        expect(decode('Maires').length).toEqual(6);
+    });
+});
+
+describe('Tests decode function', () => {
     test('if typeof decode is function', () =>{
         expect(typeof decode).toBe('function');
+    });
+
+    test(`if decode numbers '1, 2, 3, 4, 5' are converted to 'a, e, i, o, u' respectivelly`, () => {
+        expect(decode('2nc4d2')).toBe('encode');
+    });
+
+    test(`if decode numbers '0, 6, 7, 8, 9' ARE NOT converted to letters`, () => {
+        expect(encode('06789')).toBe('06789');
+    });
+
+    test('Check if the function returns a string with same length passed as a parameter', () =>{
+        expect(decode('Maires').length).toEqual(6);
     });
 });
