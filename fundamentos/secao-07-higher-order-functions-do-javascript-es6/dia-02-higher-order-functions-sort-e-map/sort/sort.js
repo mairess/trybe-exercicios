0e5ffe49dcd728865c528fe7183cfe66cbd54c38
Maires Souza <trabalho.maires@gmail.com>
2023-07-06 16:37:43 -0300

    feat: sort function

---
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-02-higher-order-functions-sort-e-map/sort/sort.js
@@ -0,0 +1,16 @@
+// Para fixar
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+
+// Utilize o sort para ordenar o array pela idade das pessoas em ordem crescente.
+  people.sort((a, b) => a.age > b.age ? 1 : -1);
+
+// Modifique o sort do exercício anterior para que ordene o array pela idade das pessoas em ordem decrescente.
+  people.sort((a, b) => b.age > a.age ? 1 : -1);
+  
+  console.log(people);
