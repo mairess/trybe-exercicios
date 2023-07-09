b037b73ab940d4f7a2bdee16efa7c4f0549c5e1d
Maires Souza <trabalho.maires@gmail.com>
2023-07-09 09:45:56 -0300

    add: comment

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios-agora-a-pratica/exercicios-agora-a-pratica.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios-agora-a-pratica/exercicios-agora-a-pratica.js
@@ -83,14 +83,14 @@ console.log(countries.reduce((totalArea, country) => totalArea + country.area, 0
 getTotalArea();
 
 // 🚀 3 - Encontre o país com o maior nome.
-const expectedResult = {
-  name: 'American Samoa',
-  region: 'Oceania',
-  currencies: [{ code: 'USD', name: 'United States Dollar' }],
-  capital: 'Pago Pago',
-  population: 55197,
-  area: 199
-}
+// const expectedResult = {
+//   name: 'American Samoa',
+//   region: 'Oceania',
+//   currencies: [{ code: 'USD', name: 'United States Dollar' }],
+//   capital: 'Pago Pago',
+//   population: 55197,
+//   area: 199
+// }
 const longestName = () => {
   console.log(countries.reduce((longestCountryName, country) => country.name.length > longestCountryName.name.length ? country: longestCountryName));
 }
