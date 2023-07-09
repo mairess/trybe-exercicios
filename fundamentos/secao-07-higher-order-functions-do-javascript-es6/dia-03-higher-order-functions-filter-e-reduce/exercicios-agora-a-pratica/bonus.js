fd3f8554105cdae4546e50c296d0175673c59787
Maires Souza <trabalho.maires@gmail.com>
2023-07-09 12:45:43 -0300

    feat: function reduceNames implemented and refactore flatten function

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios-agora-a-pratica/bonus.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios-agora-a-pratica/bonus.js
@@ -1,10 +1,79 @@
+// 1 - Dada uma matriz, transforme em um array.
 const arrays = [
     ['1', '2', '3'],
     [true],
     [4, 5, 6],
-  ];
-  
-  const flatten = () => {
-    console.log(arrays.reduce((acc, curr) => acc.concat(curr), []));
-  }
-  flatten();
+];
+
+const flatten = () => arrays.reduce((acc, curr) => acc.concat(curr), []);
+flatten()
+
+//   Para os próximos exercícios, considere o seguinte array:
+const books = [
+    {
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
+    },
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
+    },
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
+    },
+    {
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
+    },
+    {
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
+    },
+];
+
+// 2 - Crie uma string com os nomes de todas as pessoas autoras.
+// const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+const reduceNames = () => books.reduce((prev, book) => prev + `${book.author.name}, `, '')
+reduceNames()
