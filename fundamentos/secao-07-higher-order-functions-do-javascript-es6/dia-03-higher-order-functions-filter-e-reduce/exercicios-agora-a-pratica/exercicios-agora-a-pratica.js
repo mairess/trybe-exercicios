bc2700229173b3a123973e8bf476b5ada576ff63
Maires Souza <trabalho.maires@gmail.com>
2023-07-09 11:25:08 -0300

    feat: function studentAverage implemented

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios-agora-a-pratica/exercicios-agora-a-pratica.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-03-higher-order-functions-filter-e-reduce/exercicios-agora-a-pratica/exercicios-agora-a-pratica.js
@@ -102,8 +102,26 @@ const names = [
   'Abegildo', 'Adicellia', 'Aladonata',
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
-const expectedResult = 20;
+// const expectedResult = 20;
 const countA = () => {
   console.log(names.reduce((acc, curr) => acc + curr.toLowerCase().split('a').length - 1, 0));
 }
 countA();
+
+// 🚀 5 - Crie um array de objetos e calcule a média de notas. Utilize as constantes students e grades para criar um array de objetos e calcule a média da nota das pessoas estudantes:
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+// const expectedResult = [
+//  { name: 'Pedro Henrique', average: 7.8 },
+//  { name: 'Miguel', average: 9.2 },
+//  { name: 'Maria Clara', average: 8.8 },
+// ];
+const studentAverage = () => {
+  console.log(grades.map((gradeArray, index) => { 
+    const sum = gradeArray.reduce((acc, curr) => acc + curr, 0); 
+    const avarageCalc = sum / gradeArray.length; 
+    return {name: students[index], avarage: avarageCalc}}))
+}
+studentAverage()
