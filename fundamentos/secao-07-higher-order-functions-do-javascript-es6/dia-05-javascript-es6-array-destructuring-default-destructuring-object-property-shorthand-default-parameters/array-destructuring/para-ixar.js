2a1eb06fc0fb55115278a0dc8f8db44003f6a652
Maires Souza <trabalho.maires@gmail.com>
2023-07-10 16:04:22 -0300

    feat: array desctructuring for primeNumbers

---
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-default-parameters/array-destructuring/para-ixar.js
@@ -0,0 +1,11 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => {
+  console.log(a + b);
+}
+
+sum(primeNumbers[0], primeNumbers[2]) // 54
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+const [firstNumber, secondNumber, thirdNumber] = primeNumbers;
+sum(firstNumber, thirdNumber);
