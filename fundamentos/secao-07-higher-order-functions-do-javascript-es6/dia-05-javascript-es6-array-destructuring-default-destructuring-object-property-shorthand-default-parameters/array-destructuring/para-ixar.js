d2bf45e08ce821962e0ecd965316ecc78b8124d7
Maires Souza <trabalho.maires@gmail.com>
2023-07-10 17:15:43 -0300

    feat: destructuring for numerosPares

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-default-parameters/array-destructuring/para-ixar.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-default-parameters/array-destructuring/para-ixar.js
@@ -21,3 +21,12 @@ console.log(comida, animal, bebida); // arroz gato água
 // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 [comida, animal, bebida] = [bebida, comida, animal];
 console.log(comida, animal, bebida);
+
+// O array abaixo possui alguns números que não condizem com o conteúdo que ele deveria possuir. Através de array destructuring, faça com que existam apenas números pares na variável numerosPares.
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
+const [first, second, third, ...rest] = numerosPares;
+console.log(rest);
