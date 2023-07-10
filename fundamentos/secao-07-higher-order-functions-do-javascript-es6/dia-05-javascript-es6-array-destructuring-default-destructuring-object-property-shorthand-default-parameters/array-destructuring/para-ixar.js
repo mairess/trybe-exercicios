a99b2bcee7c732c0733ca4a8d9b8907da670f8ea
Maires Souza <trabalho.maires@gmail.com>
2023-07-10 16:34:10 -0300

    array destructuring for comida, animal, bebida

--- fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-default-parameters/array-destructuring/para-ixar.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-default-parameters/array-destructuring/para-ixar.js
@@ -1,3 +1,4 @@
+// Produza o mesmo resultado do código, porém utilizando o array destructuring para recolher a função e a saudação.
 const primeNumbers = [17, 23, 37]
 
 const sum = (a, b) => {
@@ -9,3 +10,14 @@ sum(primeNumbers[0], primeNumbers[2]) // 54
 // Produza o mesmo resultado acima, porém utilizando array destructuring
 const [firstNumber, secondNumber, thirdNumber] = primeNumbers;
 sum(firstNumber, thirdNumber);
+
+// A seguir, temos alguns valores que estão descritos em variáveis incoerentes. Através da desestruturação de arrays, corrija os valores das variáveis.
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+[comida, animal, bebida] = [bebida, comida, animal];
+console.log(comida, animal, bebida);
