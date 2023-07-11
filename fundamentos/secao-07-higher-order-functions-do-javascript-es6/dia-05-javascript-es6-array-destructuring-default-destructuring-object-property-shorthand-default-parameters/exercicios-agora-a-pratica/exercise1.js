7da295c836825f28b10a75d205eaf991eed614ee
Maires Souza <trabalho.maires@gmail.com>
2023-07-11 07:51:16 -0300

    feat: function swap implemented

---
+++ fundamentos/secao-07-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-array-destructuring-default-destructuring-object-property-shorthand-default-parameters/exercicios-agora-a-pratica/exercise1.js
@@ -0,0 +1,6 @@
+// Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando apenas 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5].
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
+const swap = ([a, b, c]) => [c, b, a];
+console.log(swap(myList));
