a9a06e379278ca45072fcd6aea7355de8fd3146c
Maires Souza <trabalho.maires@gmail.com>
2023-07-18 11:10:32 -0300

    feat: generate random id

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/app-gerador-de-senhas-seguras/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/app-gerador-de-senhas-seguras/src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello, wolrd!');
+import { nanoid } from 'nanoid';
+
+const btnGenerate = document.querySelector('button');
+const newPasswordHere = document.querySelector('h2');
+
+btnGenerate.addEventListener('click', () => {
+  newPasswordHere.innerHTML = nanoid();
+});
