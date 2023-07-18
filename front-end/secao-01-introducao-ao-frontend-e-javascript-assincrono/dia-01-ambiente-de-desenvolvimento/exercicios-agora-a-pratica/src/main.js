38e37d62bbdf9800908638dc6ccb5d1ea4cc299e
Maires Souza <trabalho.maires@gmail.com>
2023-07-18 16:44:41 -0300

    refactor: main

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/exercicios-agora-a-pratica/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-01-ambiente-de-desenvolvimento/exercicios-agora-a-pratica/src/main.js
@@ -3,39 +3,21 @@ import './style.css';
 
 const button = document.querySelector('button');
 const paragraph = document.querySelector('div');
+const select = document.querySelector('select');
 
 button.addEventListener('click', () => {
-  const select = document.querySelector('select');
+  const versionIpv6 = 6;
+  const versionIpv4 = 4;
+  const hash = 'sha1';
   const input = document.querySelector('input');
-  if (select.value === 'email') {
-    const validationResult = validator.isEmail(input.value);
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
-  if (select.value === 'cpf') {
-    const validationResult = validator.isNumeric(input.value);
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
-  if (select.value === 'cep') {
-    const validationResult = validator.isPostalCode(input.value, 'BR');
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
-  if (select.value === 'Ascii') {
-    const validationResult = validator.isAscii(input.value);
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
-  if (select.value === 'IPv6') {
-    const version = 6;
-    const validationResult = validator.isIP(input.value, version);
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
-  if (select.value === 'IPv4') {
-    const version = 4;
-    const validationResult = validator.isIP(input.value, version);
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
-  if (select.value === 'sha1') {
-    const hash = 'sha1';
-    const validationResult = validator.isHash(input.value, hash);
-    paragraph.innerHTML = `A validação retornou ${validationResult}`;
-  }
+  const campos = {
+    cpf: validator.isTaxID(input.value, 'pt-BR'),
+    email: validator.isEmail(input.value),
+    cep: validator.isPostalCode(input.value, 'BR'),
+    Ascii: validator.isAscii(input.value),
+    IPv6: validator.isIP(input.value, versionIpv6),
+    IPv4: validator.isIP(input.value, versionIpv4),
+    sha1: validator.isHash(input.value, hash),
+  };
+  paragraph.innerHTML = `A validação retornou ${campos[select.value]}`;
 });
