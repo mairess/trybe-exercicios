cf0986381737423882f8b7f93ad862c853cdb358
Maires Souza <trabalho.maires@gmail.com>
2023-07-20 07:44:34 -0300

    implement application

---
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
@@ -0,0 +1,8 @@
+import Swal from 'sweetalert2'
+import './style.css'
+
+const btnSortear = document.querySelector('button');
+const paragraph = document.querySelector('p');
+
+btnSortear.addEventListener('click', () => {
+});
