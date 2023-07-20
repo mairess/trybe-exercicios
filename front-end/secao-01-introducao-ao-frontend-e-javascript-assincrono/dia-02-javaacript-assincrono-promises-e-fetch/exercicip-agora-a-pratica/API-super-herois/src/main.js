63ad8df983bee15698257217f5092e8adac9f847
Maires Souza <trabalho.maires@gmail.com>
2023-07-20 08:40:26 -0300

    feat: click btn get a superhero name and image

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
@@ -3,6 +3,25 @@ import './style.css'
 
 const btnSortear = document.querySelector('button');
 const paragraph = document.querySelector('p');
+const image = document.querySelector('img');
 
 btnSortear.addEventListener('click', () => {
+    const randomId = Math.floor(Math.random() * 731) + 1;
+    const superHeroAPI = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${randomId}.json`
+    fetch(superHeroAPI)
+    .then((response) => response.json())
+    .then((data) => {
+        const { name, images: { md } } = data;
+        paragraph.innerHTML = name;
+        image.setAttribute('src', md)
+        console.log(md);
+    })
+    .catch((error) => {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Não encontrei uma super ou super, tente novamente!',
+          })
+        console.log(error.message);
+    });
 });
