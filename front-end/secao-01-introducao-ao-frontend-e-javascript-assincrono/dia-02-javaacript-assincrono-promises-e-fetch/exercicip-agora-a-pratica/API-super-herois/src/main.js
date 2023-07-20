fecf7001f9cb74464189a8cc3142d33eb5034da0
Maires Souza <trabalho.maires@gmail.com>
2023-07-20 09:09:16 -0300

    style: button

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
@@ -1,5 +1,5 @@
-import Swal from 'sweetalert2'
-import './style.css'
+import Swal from 'sweetalert2';
+import './style.css';
 
 const btnSortear = document.querySelector('button');
 const paragraph = document.querySelector('p');
