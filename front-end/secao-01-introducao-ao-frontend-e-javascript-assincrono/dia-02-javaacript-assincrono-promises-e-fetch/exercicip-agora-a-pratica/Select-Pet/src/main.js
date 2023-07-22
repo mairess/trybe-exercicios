d97c4c6f534d13d9cad736024e24011c522456e0
Maires Souza <trabalho.maires@gmail.com>
2023-07-22 18:55:13 -0300

    style: image

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/Select-Pet/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/Select-Pet/src/main.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+const randomDog = document.getElementById('randomDog');
+const randomCat = document.getElementById('randomCat');
+const surpriseMe = document.getElementById('surpriseMe');
+const randomPet = document.querySelector('img');
+
+const dogsAPI = 'https://dog.ceo/api/breeds/image/random';
+const catsAPI = 'https://api.thecatapi.com/v1/images/search';
+
+randomDog.addEventListener('click', () => {
+  fetch(dogsAPI).then((response) => response.json())
+    .then(({ message }) => {
+      randomPet.setAttribute('width', '600px');
+      randomPet.setAttribute('src', message);
+    });
+});
