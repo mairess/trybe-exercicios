9ceeaafb20a86a814b32738576dd99356fea711f
Maires Souza <trabalho.maires@gmail.com>
2023-07-22 17:42:38 -0300

    add: trybe eslint

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javaacript-assincrono-promises-e-fetch/exercicip-agora-a-pratica/API-super-herois/src/main.js
@@ -6,24 +6,25 @@ const paragraph = document.querySelector('p');
 const image = document.querySelector('img');
 
 btnSortear.addEventListener('click', () => {
-    const randomId = Math.floor(Math.random() * 731) + 1;
-    const superHeroAPI = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${randomId}.json`
-    fetch(superHeroAPI)
+  const range = 731;
+  const randomId = Math.floor(Math.random() * range) + 1;
+  const superHeroAPI = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${randomId}.json`;
+  fetch(superHeroAPI)
     .then((response) => response.json())
     .then((data) => {
-        const { name, images: { md } } = data;
-        image.style.borderRadius = '5px';
-        image.style.border = '1px solid white';
-        paragraph.innerHTML = name;
-        image.setAttribute('src', md)
-        console.log(md);
+      const { name, images: { md } } = data;
+      image.style.borderRadius = '5px';
+      image.style.border = '1px solid white';
+      paragraph.innerHTML = name;
+      image.setAttribute('src', md);
+      console.log(md);
     })
     .catch((error) => {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Não encontrei uma super ou super, tente novamente!',
-          })
-        console.log(error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Não encontrei uma super ou super, tente novamente!',
+      });
+      console.log(error.message);
     });
 });
