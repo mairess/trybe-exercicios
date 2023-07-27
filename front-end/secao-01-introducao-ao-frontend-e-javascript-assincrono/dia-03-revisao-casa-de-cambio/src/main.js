91ad6618d3900d5a68f589dda348f2e76f942630
Maires Souza <trabalho.maires@gmail.com>
2023-07-27 19:48:14 -0300

    feat: fetch the currencies

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-revisao-casa-de-cambio/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-revisao-casa-de-cambio/src/main.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+const inputData = document.querySelector('input');
+const searchBtn = document.querySelector('button');
+const API_URL = 'https://api.exchangerate.host/latest?base=';
+const board = document.getElementById('currencyBoard');
+
+const createDiv = (currencies) => {
+  Object.entries(currencies).forEach((element) => {
+    const newDiv = document.createElement('div');
+    newDiv.innerText = element;
+    board.appendChild(newDiv);
+  });
+};
+
+searchBtn.addEventListener('click', async () => {
+  board.innerHTML = ' ';
+  const currency = inputData.value;
+  const response = await fetch(API_URL + currency);
+  const data = await response.json();
+  const currencies = data.rates;
+  createDiv(currencies);
+});
