20ca5d0cdc27142cf2ef95dafc4c31b63612cb83
Maires Souza <trabalho.maires@gmail.com>
2023-07-28 11:06:16 -0300

    style: currency board

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-revisao-casa-de-cambio/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-03-revisao-casa-de-cambio/src/main.js
@@ -5,10 +5,31 @@ const searchBtn = document.querySelector('button');
 const API_URL = 'https://api.exchangerate.host/latest?base=';
 const board = document.getElementById('currencyBoard');
 
+const createImage = () => {
+  const iconPath = '../images/coins-svgrepo-com-1.svg';
+  const iconImg = document.createElement('img');
+  iconImg.src = iconPath;
+  return iconImg;
+};
+
 const createDiv = (currencies) => {
-  Object.entries(currencies).forEach((element) => {
+  Object.entries(currencies).forEach(([currency, rate]) => {
     const newDiv = document.createElement('div');
-    newDiv.innerText = element;
+    newDiv.classList.add('currencyDiv');
+
+    const iconImage = createImage();
+
+    const decimalDigits = 3;
+    const rateAdjusted = rate.toFixed(decimalDigits);
+    const rateText = document.createElement('span');
+    const currencyText = document.createElement('p');
+    currencyText.innerText = `${currency}:`;
+    rateText.innerText = `${rateAdjusted}`;
+
+    newDiv.appendChild(iconImage);
+    newDiv.appendChild(currencyText);
+    newDiv.appendChild(rateText);
+
     board.appendChild(newDiv);
   });
 };
