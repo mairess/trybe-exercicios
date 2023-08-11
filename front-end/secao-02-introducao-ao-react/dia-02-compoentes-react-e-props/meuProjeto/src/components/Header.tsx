c4dfdf6753e33e452765c2ab7bad264223ff3798
Maires Souza <trabalho.maires@gmail.com>
2023-08-11 13:09:14 -0300

    refactor: functions

--- front-end/secao-02-introducao-ao-react/dia-02-compoentes-react-e-props/meuProjeto/src/components/Header.tsx
+++ front-end/secao-02-introducao-ao-react/dia-02-compoentes-react-e-props/meuProjeto/src/components/Header.tsx
@@ -1,7 +1,12 @@
+import { NameHeader } from "./Name";
+
 function Header() {
     return (
-        <h1>Olá, Marciano Vermelho</h1>
-    )
+        <>
+        <h1>Olá,</h1> 
+        <NameHeader />
+        </>
+    );
 };
 
 export default Header;
