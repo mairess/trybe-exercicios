7d40471a6e8b4dfc38f38a984fc82b70cd87ebb0
Maires Souza <trabalho.maires@gmail.com>
2023-08-11 13:07:43 -0300

    component: Footer

---
+++ front-end/secao-02-introducao-ao-react/dia-02-compoentes-react-e-props/meuProjeto/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { NameFooter } from "./Name";
+
+function Footer(){
+    return (
+        <div className="footer-container">
+            <NameFooter />
+        </div>
+    )
+};
+
+export default Footer;
