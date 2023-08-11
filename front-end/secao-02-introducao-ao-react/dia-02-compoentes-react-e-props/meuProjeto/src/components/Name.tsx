bde858294ca3495298d96fabc8680980907e9528
Maires Souza <trabalho.maires@gmail.com>
2023-08-11 13:07:57 -0300

    component: Name

---
+++ front-end/secao-02-introducao-ao-react/dia-02-compoentes-react-e-props/meuProjeto/src/components/Name.tsx
@@ -0,0 +1,11 @@
+export function NameHeader() {
+    return (
+        <h1><span id="verde">Verde</span> <span id="amarelo">Amarelo</span></h1>
+    );
+};
+
+export function NameFooter() {
+    return (
+        <p>Acessando como <span id="verde">Verde</span> <span id="amarelo">Amarelo</span></p>
+    );
+};
