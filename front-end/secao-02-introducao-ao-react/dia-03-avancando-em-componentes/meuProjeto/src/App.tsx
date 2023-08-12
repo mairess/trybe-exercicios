937244152ea79efc8a5c7161ae55cbad2c6e0acf
Maires Souza <trabalho.maires@gmail.com>
2023-08-12 16:28:34 -0300

    feat: import image

--- front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/meuProjeto/src/App.tsx
+++ front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/meuProjeto/src/App.tsx
@@ -5,6 +5,7 @@ import Book from './components/book/';
 import Title from './components/title/';
 import { booksList, bestMovie } from './data';
 // import * as bookData from './data';
+import luminariaSRC from './assets/luminaria-src.webp';
 
 function App() {
   return (
@@ -28,6 +29,12 @@ function App() {
           </ul>
           <Title>{bestMovie}</Title>
           {/* <Title>{bookData.bestMovie}</Title> */}
+
+          <div className='images-container'>
+            <img src={luminariaSRC} alt='foto luminaria cor preta em fundo branco' />
+            <img src="/luminaria-public.jpeg" alt='foto luminaria cor preta em fundo branco modelo 2' />
+          </div>
+
         </div>
         <Footer />
       </div>
