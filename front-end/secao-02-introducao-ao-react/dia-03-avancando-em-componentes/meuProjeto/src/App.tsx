352c4f5821ddf1d9919521eae8f85d91b046d87e
Maires Souza <trabalho.maires@gmail.com>
2023-08-12 15:55:42 -0300

    import data

--- front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/meuProjeto/src/App.tsx
+++ front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/meuProjeto/src/App.tsx
@@ -3,97 +3,8 @@ import Header from './components/header/';
 import Footer from './components/footer/';
 import Book from './components/book/';
 import Title from './components/title/';
-
-const booksList = [
-  {
-    title: "The Enchanted Forest",
-    author: "Alice Writer",
-    pages: 320,
-    isRead: true,
-    isFavorite: false
-  },
-  {
-    title: "Secrets of the Starlight",
-    author: "Bob Authorson",
-    pages: 240,
-    isRead: false,
-    isFavorite: false
-  },
-  {
-    title: "Mysteries of the Moonshadow",
-    author: "Charlie Novelist",
-    pages: 320,
-    isRead: true,
-    isFavorite: false
-  },
-  {
-    title: "Enigma of the Emerald Isle",
-    author: "David Wordsmith",
-    pages: 180,
-    isRead: false,
-    isFavorite: false
-  },
-  {
-    title: "Whispers in the Wind",
-    author: "Eve Storyteller",
-    pages: 200,
-    isRead: true,
-    isFavorite: false
-  },
-  {
-    title: "Chronicles of the Cosmos",
-    author: "Frank Narrator",
-    pages: 400,
-    isRead: false,
-    isFavorite: false
-  },
-  {
-    title: "Legends of Luminaria",
-    author: "Grace Wordsworth",
-    pages: 280,
-    isRead: true,
-    isFavorite: true
-  },
-  {
-    title: "Quest for the Quill",
-    author: "Henry Scribbler",
-    pages: 150,
-    isRead: false,
-    isFavorite: false
-  },
-  {
-    title: "Sagas of the Sunchasers",
-    author: "Isabel Penman",
-    pages: 260,
-    isRead: false,
-    isFavorite: false
-  },
-
-  {
-    title: "Echoes from Eternity",
-    author: "Jack Lyricist",
-    pages: 310,
-    isRead: false,
-    isFavorite: false
-  },
-
-  {
-    title: "Tales of the Twilight Realm",
-    author: "Kate Scribe",
-    pages: 230,
-    isRead: false,
-    isFavorite: false
-  },
-
-  {
-    title: "Riddles of the Radiant Orb",
-    author: "Leo Mythos",
-    pages: 190,
-    isRead: false,
-    isFavorite: false
-  },
-
-];
+import { booksList, bestMovie } from './data';
+// import * as bookData from './data';
 
 function App() {
   return (
@@ -111,7 +22,12 @@ function App() {
             {booksList.map((book) => (
               <Book key={book.title} book={book} />
             ))}
+            {/* {bookData.booksList.map((book) => (
+              <Book key={book.title} book={book} />
+            ))} */}
           </ul>
+          <Title>{bestMovie}</Title>
+          {/* <Title>{bookData.bestMovie}</Title> */}
         </div>
         <Footer />
       </div>
