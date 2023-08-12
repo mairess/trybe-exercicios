a97f881a29f9b2d273bc1e1f4a2dfb61205cda0d
Maires Souza <trabalho.maires@gmail.com>
2023-08-12 16:34:33 -0300

    add: react icon

--- front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/meuProjeto/src/components/name/index.tsx
+++ front-end/secao-02-introducao-ao-react/dia-03-avancando-em-componentes/meuProjeto/src/components/name/index.tsx
@@ -1,4 +1,5 @@
 import "./name.css";
+import { AiOutlineMail } from "react-icons/ai";
 
 export function NameHeader() {
   return (
@@ -15,7 +16,7 @@ export function NameFooter() {
     <footer className="footer">
       <p>
         Acessando como <span id="verde">Verde</span>{" "}
-        <span id="amarelo">Amarelo</span> Direitos reservados à <a className="header-user" id="trybe-link" href="https://www.betrybe.com/" target="blank">Trybe</a>
+        <span id="amarelo">Amarelo</span> Direitos reservados à <a className="header-user" id="trybe-link" href="https://www.betrybe.com/" target="blank">Trybe <AiOutlineMail/></a>
       </p>
     </footer>
   );
