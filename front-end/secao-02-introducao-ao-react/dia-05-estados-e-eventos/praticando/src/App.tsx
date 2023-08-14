7a61d87af895a4a7e72af79339ea7574ef835040
Maires Souza <trabalho.maires@gmail.com>
2023-08-14 15:43:20 -0300

    add: carrossel

---
+++ front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/praticando/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+function App() {
+
+  const toolKit = [
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'HTML',
+    'CSS',
+    'Node',
+    'Testes automatizados',
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  function handleClickNext () {
+    // setIndex((prevIndex) => (prevIndex + 1) % toolKit.length);
+    if (index + 1 < toolKit.length) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handleClickPrevious () {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+    } else {
+      setIndex(toolKit.length - 1);
+    }
+    // setIndex((prevIndex) => (prevIndex - 1 + toolKit.length) % toolKit.length);
+  }
+
+  return (
+    <>
+      <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
+      <h2>{ toolKit[index] }</h2>
+      <button onClick={handleClickPrevious}>Anterior</button>
+      <button onClick={handleClickNext}>Próximo</button>
+    </>
+  );
+}
+
+export default App;
+
