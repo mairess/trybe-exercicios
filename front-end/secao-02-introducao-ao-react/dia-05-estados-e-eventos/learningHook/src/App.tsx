33489216b9cdb0c1984604584ef9435487319a21
Maires Souza <trabalho.maires@gmail.com>
2023-08-14 14:17:07 -0300

    add useState

--- front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/learningHook/src/App.tsx
+++ front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/learningHook/src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css'
 
+// src/App.tsx
+
+import { useState } from 'react';
+
 function App() {
+  const [showImg, setShowImg] = useState(true);
+
+  function handleClick() {
+    setShowImg(!showImg)
+  }
 
   return (
-    <>
-      <div>
-        oi
-      </div>
-    </>
-  )
+    <div>
+    {showImg && <img src="/trybe-icon-filled-256.png" />}
+    <button onClick={handleClick}>
+      {showImg ? 'Esconder imagem' : 'Mostrar imagem'}
+    </button>
+  </div>
+  );
 }
 
-export default App
+export default App;
+
