8113ecb13910f3dfef4208d70b2cede4ab6f1a56
Maires Souza <trabalho.maires@gmail.com>
2023-08-14 12:40:18 -0300

    passing args to event handler

--- front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/event-handler/src/App.tsx
+++ front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/event-handler/src/App.tsx
@@ -5,39 +5,72 @@ import { Button, Button2 } from './conponents/Button';
 import ButtonPropChildren from './conponents/button';
 
 function App() {
-    function handleClick () {
-        return alert('Clicado propchildren man!');
-    }
+
+  function handleClick(messageText: string) {
+    return alert(messageText);
+  }
+
+  function handleClick1() {
+    return alert('Clicado propchildren man!');
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    return console.log(event.target.value);
+  }
+
   return (
     <>
       <div>
-          <Header />
-          <div>
-            <button onClick={() => alert('Botão onClick não é componente meu bom!')}>
-              Clica aqui mano
-            </button>
+        <Header />
+        <div>
+          <button onClick={() => alert('Botão onClick não é componente meu bom!')}>
+            Clica aqui mano
+          </button>
 
-            <br />
-            <br />
+          <br />
+          <br />
 
-            <Button />
+          <Button />
 
-            <br />
-            <br />
-            
-            <Button2 />
+          <br />
+          <br />
 
-            <br />
-            <br />
+          <Button2 />
 
-            <ButtonPropChildren 
-             onClick={handleClick}
-            >
-              Botão prop
-            </ButtonPropChildren>
+          <br />
+          <br />
 
-          </div>
-          <Footer />
+          <ButtonPropChildren
+            onClick={handleClick1}
+          >
+            Botão prop
+          </ButtonPropChildren>
+
+          <br />
+          <br />
+
+          <ButtonPropChildren
+            onClick={() => handleClick('Botão 1')}
+          >
+            passing param to hdl click 1
+          </ButtonPropChildren>
+
+          <br />
+          <br />
+
+          <ButtonPropChildren
+            onClick={() => handleClick('Botão 2')}
+          >
+            passing param to hdl click 2
+          </ButtonPropChildren>
+
+          <br />
+          <br />
+
+          <input type="text" onChange={(event) => handleChange(event)} />
+
+        </div>
+        <Footer />
       </div>
     </>
   )
