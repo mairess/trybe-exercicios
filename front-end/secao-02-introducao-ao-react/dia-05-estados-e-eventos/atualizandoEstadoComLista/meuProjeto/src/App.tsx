155636df9f4ce7a2c5dabe5ab220f445c791e4f7
Maires Souza <trabalho.maires@gmail.com>
2023-08-15 08:22:39 -0300

    init project

---
+++ front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/atualizandoEstadoComLista/meuProjeto/src/App.tsx
@@ -0,0 +1,78 @@
+import './App.css'
+import Header from './components/header/';
+import Footer from './components/footer/';
+import Book from './components/book/';
+import Title from './components/title/';
+import { booksList, bestMovie } from './data';
+import { useState } from 'react';
+// import * as bookData from './data';
+
+function App() {
+
+const [bookTitle, setBookTitle] = useState("");
+const [bookPages, setBookPages] = useState(0);
+const [bookAuthor, setBookAuthor] = useState("");
+const [books, setBooks] = useState(booksList);
+
+function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+  setBookTitle(event.target.value);
+  }
+
+function handleAuthorChange(event: React.ChangeEvent<HTMLInputElement>) {
+  setBookAuthor(event.target.value);
+}
+
+function handlePagesChange(event: React.ChangeEvent<HTMLInputElement>) {
+  setBookPages(event.target.valueAsNumber);
+  }
+
+function handleAddClick() {
+  const newBook = {
+    title: bookTitle,
+    author: bookAuthor,
+    pages: bookPages,
+    isRead: false,
+    isFavorite: false,
+  }
+
+  setBooks([newBook]);
+
+  }
+
+  return (
+    <>
+      <div className='app'>
+        <Header />
+        <div className="container">
+          <Title>
+            <p>
+              <s>Livros emprestados </s>
+              Meus livros:
+            </p>
+          </Title>
+          <ul className='books-list'>
+            {books.map((book) => (
+              <Book key={book.title} book={book} />
+            ))}
+            {/* {bookData.booksList.map((book) => (
+              <Book key={book.title} book={book} />
+            ))} */}
+          </ul>
+          <Title>{bestMovie}</Title>
+          {/* <Title>{bookData.bestMovie}</Title> */}
+
+          <div className='images-container'>
+            <input onChange={handleNameChange} id="title" type="text" value={bookTitle} placeholder="Título" />
+            <input onChange={handleAuthorChange} id="author" type="text" value={bookAuthor} placeholder="Autor" />
+            <input onChange={handlePagesChange} id="paginas" type="number" value={bookPages} placeholder="Quantidade de páginas" />
+            <button onClick={handleAddClick}>Adicionar</button>
+          </div>
+
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ front-end/secao-02-introducao-ao-react/dia-05-estados-e-eventos/atualizandoEstadoComLista/meuProjeto/src/data.ts
@@ -0,0 +1,92 @@
+export const booksList = [
+    {
+      title: "The Enchanted Forest",
+      author: "Alice Writer",
+      pages: 320,
+      isRead: true,
+      isFavorite: false
+    },
+    {
+      title: "Secrets of the Starlight",
+      author: "Bob Authorson",
+      pages: 240,
+      isRead: false,
+      isFavorite: false
+    },
+    {
+      title: "Mysteries of the Moonshadow",
+      author: "Charlie Novelist",
+      pages: 320,
+      isRead: true,
+      isFavorite: false
+    },
+    {
+      title: "Enigma of the Emerald Isle",
+      author: "David Wordsmith",
+      pages: 180,
+      isRead: false,
+      isFavorite: false
+    },
+    {
+      title: "Whispers in the Wind",
+      author: "Eve Storyteller",
+      pages: 200,
+      isRead: true,
+      isFavorite: false
+    },
+    {
+      title: "Chronicles of the Cosmos",
+      author: "Frank Narrator",
+      pages: 400,
+      isRead: false,
+      isFavorite: false
+    },
+    // {
+    //   title: "Legends of Luminaria",
+    //   author: "Grace Wordsworth",
+    //   pages: 280,
+    //   isRead: true,
+    //   isFavorite: true
+    // },
+    // {
+    //   title: "Quest for the Quill",
+    //   author: "Henry Scribbler",
+    //   pages: 150,
+    //   isRead: false,
+    //   isFavorite: false
+    // },
+    // {
+    //   title: "Sagas of the Sunchasers",
+    //   author: "Isabel Penman",
+    //   pages: 260,
+    //   isRead: false,
+    //   isFavorite: false
+    // },
+  
+    // {
+    //   title: "Echoes from Eternity",
+    //   author: "Jack Lyricist",
+    //   pages: 310,
+    //   isRead: false,
+    //   isFavorite: false
+    // },
+  
+    // {
+    //   title: "Tales of the Twilight Realm",
+    //   author: "Kate Scribe",
+    //   pages: 230,
+    //   isRead: false,
+    //   isFavorite: false
+    // },
+  
+    // {
+    //   title: "Riddles of the Radiant Orb",
+    //   author: "Leo Mythos",
+    //   pages: 190,
+    //   isRead: false,
+    //   isFavorite: false
+    // },
+  
+  ];
+
+export const bestMovie =  'Legends of Luminaria';
