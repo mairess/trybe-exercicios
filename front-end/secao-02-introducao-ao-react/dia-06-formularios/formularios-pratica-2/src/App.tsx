9b5e8ba09bad542082f4d32503dcadaf63ab868b
Maires Souza <trabalho.maires@gmail.com>
2023-08-16 08:52:32 -0300

    hook: useState

--- front-end/secao-02-introducao-ao-react/dia-06-formularios/formularios-pratica-2/src/App.tsx
+++ front-end/secao-02-introducao-ao-react/dia-06-formularios/formularios-pratica-2/src/App.tsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
+
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [schooling, setSchooling] = useState('Médio');
+  const [resume, setResume] = useState('');
+
   return (
     <form>
       <label>
         Nome
-        <input />
+        <input value={ name } onChange={ ({ target }) => setName(target.value) } />
       </label>
       <label>
         E-mail
-        <input />
+        <input value={ email } onChange={ ({ target }) => setEmail(target.value) } />
       </label>
       <label>
         Escolaridade
-        <select>
+        <select
+          value={ schooling }
+          onChange={ ({ target }) => setSchooling(target.value) }
+        >
           <option value="Médio">Médio</option>
           <option value="Superior">Superior</option>
           <option value="Pós-graduação">Pós-graduação</option>
@@ -19,7 +29,7 @@ function App() {
       </label>
       <label>
         Resumo das experiências
-        <textarea />
+        <textarea value={ resume } onChange={ ({ target }) => setResume(target.value) } />
       </label>
       <button>Enviar</button>
     </form>
