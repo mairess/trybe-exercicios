7062e1c9c75d4631fe2d043111b00d8ec6746b5e
Maires Souza <trabalho.maires@gmail.com>
2023-09-04 08:59:54 -0300

    initial commit

---
+++ front-end/secao-04-testes-automatizados-com-react-testing-library/dia-01-introducao-a-react-esting-library/Learning-RTL/src/App.test.tsx
@@ -0,0 +1,10 @@
+// src/App.test.tsx
+
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+it('should show "Vite + React" text', () => {
+  render(<App />);
+  expect(screen.getByText(/vite \+ react/i)).toBeInTheDocument();
+})
