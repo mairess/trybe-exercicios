310d24161634de5429561f4f03ec59305235b0dc
Maires Souza <trabalho.maires@gmail.com>
2023-09-04 10:47:04 -0300

    add: label

--- front-end/secao-04-testes-automatizados-com-react-testing-library/dia-01-introducao-a-react-esting-library/RTL-Na-Pratica/src/App.tsx
+++ front-end/secao-04-testes-automatizados-com-react-testing-library/dia-01-introducao-a-react-esting-library/RTL-Na-Pratica/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import './App.css'
-import Back from './BackBtn'
-import Input from './Input'
-import Send from './SendBtn'
-import Value from './Value'
+import Back from './components/BackBtn'
+import Input from './components/Input'
+import Send from './components/SendBtn'
+import Value from './components/Value'
 
 function App() {
   const [inputMail, setInputMail] = useState<string>('');
