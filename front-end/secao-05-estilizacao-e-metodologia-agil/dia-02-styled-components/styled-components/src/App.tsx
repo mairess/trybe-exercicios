40031aedef596965f816f9a5322c7d8dce43d882
Maires Souza <trabalho.maires@gmail.com>
2023-09-13 09:51:52 -0300

    style: reset previous styling

--- front-end/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/styled-components/src/App.tsx
+++ front-end/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/styled-components/src/App.tsx
@@ -1,25 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import styled from "styled-components";
+// import './App.css'
+import {Header, Container} from "./styles.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const Header = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 1em;
-    background-color: #022c16;
-    margin-bottom: 3em;
-  `
-  const Container = styled.div`
-    width: 80%;
-    max-width: 800px;
-    margin: 0 auto;
-    background-color: #022c16;
-  `
+
 
   return (
    <>
