c6af5514ad119d0cff165fa724dea931068526ba
Maires Souza <trabalho.maires@gmail.com>
2023-09-13 10:30:43 -0300

    add: untracked styles.tsx and update readme

---
+++ front-end/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/styled-components/src/styles.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 1em;
+    background-color: #022c16;
+    margin-bottom: 3em;
+  `
+export const Container = styled.div`
+    width: 80%;
+    max-width: 800px;
+    margin: 0 auto;
+    background-color: #022c16;
+  `
