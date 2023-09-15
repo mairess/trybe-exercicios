9b74669b1102cf044965145dab5a303196f98dc6
Maires Souza <trabalho.maires@gmail.com>
2023-09-15 10:20:49 -0300

    feat: add global style

--- front-end/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/styled-components/src/App.tsx
+++ front-end/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/styled-components/src/App.tsx
@@ -1,11 +1,27 @@
 // import './App.css'
 import {Header, Container} from "./styles.tsx";
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  background-color: #333333;
+}
+
+h1 {
+  color: white;
+}
+`;
 
   return (
    <>
+   <GlobalStyle />
    <Header>
     <h1>Título</h1>
    </Header>
