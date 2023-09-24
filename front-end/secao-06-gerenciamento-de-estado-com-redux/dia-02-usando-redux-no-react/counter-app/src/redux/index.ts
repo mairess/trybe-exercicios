6cc28125f7a744d3187ff1c42ddbb2dc7d9cbb65
Maires Souza <trabalho.maires@gmail.com>
2023-09-24 20:13:16 -0300

    feat: extracting reducer

--- front-end/secao-06-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/counter-app/src/redux/index.ts
+++ front-end/secao-06-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/counter-app/src/redux/index.ts
@@ -1,25 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
-// import counterReducer from "./reducer/counterReducer";
+import counterReducer from "./reducer/counterReducer";
 
-type ActionType = {
-    type: string;
-    payload: number;
-}
-
-const INITIAL_STATE = { count: 0 };
-
-const reducer = (state = INITIAL_STATE, action: ActionType) => {
-    switch (action.type) {
-        case 'INCREMENT_COUNTER': {
-            return {
-                count: state.count + action.payload,
-            };
-        }
-        default: return state;
-    }
-};
-
-const store = createStore(reducer, composeWithDevTools());
+const store = createStore(counterReducer, composeWithDevTools());
 
 export default store;
