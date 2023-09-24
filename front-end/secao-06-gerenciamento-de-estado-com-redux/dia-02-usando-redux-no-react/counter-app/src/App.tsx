1f96ba3dc74fca51029d725020217b46fcc22cb0
Maires Souza <trabalho.maires@gmail.com>
2023-09-24 10:09:29 -0300

    add: hook useSelector

--- front-end/secao-06-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/counter-app/src/App.tsx
+++ front-end/secao-06-gerenciamento-de-estado-com-redux/dia-02-usando-redux-no-react/counter-app/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import { useSelector } from 'react-redux';
+
+type RootState = {
+  count: number;
+}
 
 function App() {
+  const rootState = useSelector((state: RootState) => state);
+
   return (
     <div>
       <h1>Contador</h1>
-      <h2>0</h2>
-      <button>Incrementar</button>
+      <h2>{rootState.count}</h2>
+      <button>Incrementa 1</button>
+      <button>Incrementa 5</button>
     </div>
   )
 }
