6dd89de585b5077eeed5e10b056a371710e35114
Maires Souza <trabalho.maires@gmail.com>
2023-10-03 18:06:20 -0300

    add props and state

--- front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-01-componentes-e-classes/componentes-e-clases/src/App.tsx
+++ front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-01-componentes-e-classes/componentes-e-clases/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css'
+import RandomNumber from './componets/random-number';
 
 class App extends React.Component {
 render() {
     return (
-        <h1>Hello world</h1>
+        <RandomNumber 
+        max={ 5 }
+        />
     )
 }
 }
