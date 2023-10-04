69dc43c510ec65236ffc1f09d662e2f48399ecbe
Maires Souza <trabalho.maires@gmail.com>
2023-10-04 07:55:32 -0300

    feat: implement button to hide component

--- front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-01-componentes-e-classes/componentes-e-clases/src/App.tsx
+++ front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-01-componentes-e-classes/componentes-e-clases/src/App.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import './App.css'
 import RandomNumber from './componets/random-number';
 
-class App extends React.Component {
-render() {
-    return (
-        <RandomNumber 
-        max={ 5 }
-        />
-    )
-}
+type AppState = {
+    showRandomNumber: boolean,
+};
+class App extends React.Component<object, AppState> {
+    state = {
+        showRandomNumber: false,
+    }
+
+    handleClick = () => {
+        this.setState((prevState) => (
+            { showRandomNumber: !prevState.showRandomNumber }
+        ));
+    };
+    render() {
+        const { showRandomNumber } = this.state;
+        return (
+            <>
+                <button
+                    onClick={this.handleClick}
+                >
+                    {showRandomNumber ? 'hide' : 'show'}
+                </button>
+                {showRandomNumber
+                    ? (<RandomNumber max={10} />)
+                    : ( <h1>?</h1> )}
+            </>
+        )
+    }
 }
 
 export default App;
