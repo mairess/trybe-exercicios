d91b42060509c1afdb0713fe401db4ef03f4b04f
Maires Souza <trabalho.maires@gmail.com>
2023-10-05 15:14:44 -0300

    feat: reading context

--- front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-02-context-API/criando-context-api/src/components/Footer.tsx
+++ front-end/secao-07-componentes-de-classes-context-api-e-custom-hooks/dia-02-context-API/criando-context-api/src/components/Footer.tsx
@@ -1,7 +1,14 @@
 import '../style.css';
+import { useContext } from 'react';
+import ThemeContext from '../context/ThemeContext';
 
 export default function Footer() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <footer>Tema atual:</footer>
+    <footer>
+      Tema atual:
+      {' '}
+      {theme}
+    </footer>
   );
 }
