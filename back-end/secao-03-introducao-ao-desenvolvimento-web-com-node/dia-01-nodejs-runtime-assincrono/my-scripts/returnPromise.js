de0908c8463be64ca8b8408343264fa40724eccf
mairess <trabalho.maires@gmail.com>
2023-11-29 18:31:53 -0300

    feat: implement functiuon returnPromise

---
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-01-nodejs-runtime-assincrono/my-scripts/returnPromise.js
@@ -0,0 +1,20 @@
+function returnPromise(param1, param2, param3) {
+    const promise = new Promise((resolve, reject) => {
+        if (isNaN(param1) || isNaN(param2) || isNaN(param3)) {
+            reject(new Error('Informe apenas números'));
+        }
+
+        const result = (param1 + param2) * param3;
+
+        if (result < 50) reject(new Error('Valor muito baixo'));
+
+        resolve(result);
+
+    });
+
+    return promise;
+};
+
+returnPromise(1, 1, 1)
+    .then((result) => result)
+    .catch((error) => console.log(`Erro: %s`, error.message));;
