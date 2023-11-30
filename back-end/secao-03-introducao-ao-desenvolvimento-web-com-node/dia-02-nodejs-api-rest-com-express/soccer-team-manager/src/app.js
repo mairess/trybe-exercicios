051e5d0634e5715443f0e789d46b901e1c4280d4
mairess <trabalho.maires@gmail.com>
2023-11-30 10:43:10 -0300

    feat: add new route

--- back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,14 +1,48 @@
 const express = require('express');
-const data = require('./data');
+const teams = require('./teams');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.status(200).json({ message: 'olá mundo!' }));
 
-app.get('/cabron', (req, res) => res.status(200).json({ message: data }));
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
 
 app.get('/hello-world', (req, res) => res.send('<h1>Olá Mundo!</h1>'));
 
 app.get('/go-trybe', (req, res) => res.redirect('https://www.betrybe.com'));
+
+app.post('/teams', (req, res) => {
+    const newTeam = { ...req.body };
+    teams.push(newTeam);
+    res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, initials } = req.body;
+
+    const updateTeam = teams.find((team) => team.id === Number(id));
+    
+    if (!updateTeam) {
+        return res.status(404).json({ message: 'Team not found!' });
+    }
+
+    updateTeam.name = name;
+    updateTeam.initials = initials;
+    res.status(200).json({ updateTeam });
+});
+
+app.get('/teams/:id', (req, res) => {
+    const { id } = req.params;
+    const theTeam = teams.find((team) => team.id === Number(id));
+
+    if (!theTeam) {
+        return res.status(404).json({ message: 'Team not found!' });
+    }
+
+    res.status(200).json(theTeam);
+});
 
 module.exports = app;
