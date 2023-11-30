046305b30ce275d98f1927a8311fa60db79ba000
mairess <trabalho.maires@gmail.com>
2023-11-30 15:59:52 -0300

    feat: implement writeFileASync function

---
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/exercice/src/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const readFileAsync = require('./readFileAsync');
+const writeFileASync = require('./writeFileASync');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/movies', async (req, res) => {
+    const movies = await readFileAsync();
+    res.status(200).json({movies})
+});
+
+app.get('/movies/:id', async (req, res) => {
+    const { id } = req.params;
+    const movies = await readFileAsync();
+
+    const movie = movies.find((movie) => movie.id === Number(id));
+
+    res.status(200).json({movie});
+});
+
+app.post('/movies', async (req, res) => {
+    const newMovie = {...req.body};
+    const movies = await readFileAsync();
+    movies.push(newMovie);
+    await writeFileASync(movies)
+    res.status(201).json({newMovie});
+})
+
+module.exports = app;
