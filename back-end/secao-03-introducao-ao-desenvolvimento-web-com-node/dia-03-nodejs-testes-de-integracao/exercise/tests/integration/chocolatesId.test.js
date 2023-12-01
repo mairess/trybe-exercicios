6cc61558312bcf07f62de8507e41064753e4c252
mairess <trabalho.maires@gmail.com>
2023-12-01 17:25:20 -0300

    add: sinon

--- back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-03-nodejs-testes-de-integracao/exercise/tests/integration/chocolatesId.test.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-03-nodejs-testes-de-integracao/exercise/tests/integration/chocolatesId.test.js
@@ -2,13 +2,55 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const readChocolates = require('../../src/getAllChocalates');
 const app = require('../../src/app')
+const sinon = require('sinon');
+const fs = require('fs');
 
 const { expect } = chai;
 
 chai.use(chaiHttp);
 
+const mockFile = JSON.stringify({
+    "brands": [
+        {
+            "id": 1,
+            "name": "Lindt & Sprungli"
+        },
+        {
+            "id": 2,
+            "name": "Ferrero"
+        },
+        {
+            "id": 3,
+            "name": "Ghirardelli"
+        }
+    ],
+    "chocolates": [
+        {
+            "id": 1,
+            "name": "Mint Intense",
+            "brandId": 1
+        },
+        {
+            "id": 2,
+            "name": "White Coconut",
+            "brandId": 1
+        },
+        {
+            "id": 3,
+            "name": "Mon Chéri",
+            "brandId": 2
+        },
+        {
+            "id": 4,
+            "name": "Mounds",
+            "brandId": 3
+        }
+    ]
+});
+
 describe('Testa a API cacau Trybe com mais rotas.', function() {
     it('Usando o método GET em /chocolates/total para buscar o total de chocolates.', async function() {
+       sinon.stub(fs.promises, 'readFile').resolves(mockFile)
         const output = [
             { "id": 1, "name": "Mint Intense", "brandId": 1 },
             { "id": 2, "name": "White Coconut", "brandId": 1 },
