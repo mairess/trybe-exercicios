52fb49c370e0227b92cc274b66b9d2cb6e5a1bf9
mairess <trabalho.maires@gmail.com>
2023-12-01 06:53:32 -0300

    feat: endpoit /movies/search with query

--- back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/exercice/src/app.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/exercice/src/app.js
@@ -1,49 +1,55 @@
 const express = require('express');
 const readFileAsync = require('./readFileAsync');
 const writeFileASync = require('./writeFileASync');
-// const updateFileAsync = require('./updateFileAsync');
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/movies/search', async (req, res) => {
+    const { q } = req.query;
+    const movies = await readFileAsync();
+    
+    const filteredData = movies.filter((m) => m.movie.toLowerCase() === q.toLowerCase());
+    
+    res.status(200).json(filteredData);
+});
+
 app.get('/movies', async (req, res) => {
     const movies = await readFileAsync();
-    res.status(200).json({movies})
+    res.status(200).json({ movies });
 });
 
 app.get('/movies/:id', async (req, res) => {
     const { id } = req.params;
     const movies = await readFileAsync();
 
-    const movie = movies.find((movie) => movie.id === Number(id));
+    const movie = movies.find((m) => m.id === Number(id));
 
-    res.status(200).json({movie});
+    res.status(200).json({ movie });
 });
 
 app.post('/movies', async (req, res) => {
-    const newMovie = {...req.body};
+    const newMovie = { ...req.body };
     const movies = await readFileAsync();
     const maxId = Math.max(...movies.map((movie) => movie.id));
     newMovie.id = maxId + 1;
     movies.push(newMovie);
-    await writeFileASync(movies)
-    res.status(201).json({newMovie});
-})
+    await writeFileASync(movies);
+    res.status(201).json({ newMovie });
+});
 
 app.put('/movies/:id', async (req, res) => {
     const { id } = req.params;
     const { movie, price } = req.body;
     const movies = await readFileAsync();
 
-    const movieIndex = movies.findIndex((movie) => movie.id === Number(id));
+    const movieIndex = movies.findIndex((m) => m.id === Number(id));
 
     movies[movieIndex] = { id: Number(id), movie, price };
-    // const updatedMovies = JSON.stringify(movies, null, 2);
     await writeFileASync(movies);
 
     res.status(200).json({ movie: movies[movieIndex] });
 });
-
 
 module.exports = app;
--- back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/exercice/src/readFileAsync.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-02-nodejs-api-rest-com-express/exercice/src/readFileAsync.js
@@ -2,16 +2,15 @@ const fs = require('fs').promises;
 const path = require('path');
 
 async function readFileAsync() {
-    try{
+    try {
         const filePath = path.resolve(__dirname, 'movies.json');
         const data = await fs.readFile(filePath, 'utf-8');
         const parsed = JSON.parse(data);
-        return parsed
-    } catch(error) {
+        return parsed;
+    } catch (error) {
         console.error(`Erro ao ler o arquivo: ${error.message}`);
     }
-
-};
+}
 
 readFileAsync();
 
