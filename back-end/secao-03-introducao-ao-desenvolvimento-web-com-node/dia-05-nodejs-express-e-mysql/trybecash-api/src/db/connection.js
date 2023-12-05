d40e77b7407fbc0dbf4bb815f5fa5d832d293ca0
mairess <trabalho.maires@gmail.com>
2023-12-05 09:31:12 -0300

    feat: envi

--- back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-05-nodejs-express-e-mysql/trybecash-api/src/db/connection.js
+++ back-end/secao-03-introducao-ao-desenvolvimento-web-com-node/dia-05-nodejs-express-e-mysql/trybecash-api/src/db/connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 33060,
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'trybecashdb',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE_NAME,
+  waitForConnections: process.env.MYSQL_WAIT_FOR_CONNECTIONS,
+  connectionLimit: process.env.MYSQL_CONNECTION_LIMIT,
+  queueLimit: process.env.MYSQL_QUEUE_LIMIT,
 });
 
 module.exports = connection;
