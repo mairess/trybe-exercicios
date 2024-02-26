496a3e9729779c41c00cf11a0bbf7ba5609fbf2d
mairess <trabalho.maires@gmail.com>
2024-02-26 10:00:14 -0300

    feat: composition added

---
+++ back-end/secao-08-programacao-orientada-a-objetos-poo-solid/dia-02-heranca-e-composicao/composition.composition.ts.ts
@@ -0,0 +1,41 @@
+class Profile {
+    private name: string;
+    private email: string;
+
+    constructor(name: string, email: string) { 
+        this.name = name;
+        this.email = email;
+    };
+  
+    public setName(name: string) {
+      this.name = name;
+    }
+  
+    public setEmail(email: string) {
+      this.email = email;
+    }
+  
+    public toString(): string {
+      return `name - [${this.name}] - email - [${this.email}]`;
+    }
+  }
+  
+  class SocialMediaAccount {
+    // Cria nossa composição com o perfil
+    private profile = new Profile('amsdasd', 'asdad');
+  
+    public editProfile(name: string, email: string) {
+      this.profile.setName(name);
+      this.profile.setEmail(email);
+    }
+  
+    public printProfile() {
+      console.log(this.profile.toString());
+    }
+  }
+  
+  Function
+  
+  const socialMediaAccount = new SocialMediaAccount();
+  socialMediaAccount.editProfile('Manuella', 'manu@trybe.com');
+  socialMediaAccount.printProfile();
