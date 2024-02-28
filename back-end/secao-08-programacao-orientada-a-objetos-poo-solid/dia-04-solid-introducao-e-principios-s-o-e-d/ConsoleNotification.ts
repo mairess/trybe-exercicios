90a7ef03ab5167c3cc9b0e314b5560a65c7b418e
mairess <trabalho.maires@gmail.com>
2024-02-28 10:30:07 -0300

    exercise

---
+++ back-end/secao-08-programacao-orientada-a-objetos-poo-solid/dia-04-solid-introducao-e-principios-s-o-e-d/ConsoleNotification.ts
@@ -0,0 +1,9 @@
+import Notificator from './Notificator';
+
+export default class ConsoleNotification implements Notificator {
+  constructor(private name: string) { }
+
+  sendNotification(message: string): void {
+    console.log(`Here we go again! - ${message} from ${this.name}`);
+  }
+}
---
+++ back-end/secao-08-programacao-orientada-a-objetos-poo-solid/dia-04-solid-introducao-e-principios-s-o-e-d/EmailNotification.ts
@@ -0,0 +1,16 @@
+import Notificator from './Notificator';
+
+export default class EmailNotification implements Notificator {
+  private email: string;
+
+  constructor(email: string) {
+    this.email = email;
+  }
+
+  sendNotification(message: string): void {
+    console.log(
+      `Here should go the implementation \
+      to send notification to the email: ${this.email} - ${message}`,
+    );
+  }
+}
