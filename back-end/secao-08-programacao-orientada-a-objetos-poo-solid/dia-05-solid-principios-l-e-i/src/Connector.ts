c7ed48c72b7fdd456494706d47ba586eebeb270c
mairess <trabalho.maires@gmail.com>
2024-02-29 08:22:45 -0300

    initial config

---
+++ back-end/secao-08-programacao-orientada-a-objetos-poo-solid/dia-05-solid-principios-l-e-i/src/Connector.ts
@@ -0,0 +1,58 @@
+import mysql, { Connection } from 'mysql';
+
+interface ConnectorConstructor {
+  host: string;
+  port: number;
+  database?: string;
+  user?: string;
+  password?: string;
+}
+
+export default class MySQLConnector {
+  private connection: Connection;
+
+  constructor(config: ConnectorConstructor) {
+    this.connection = mysql.createConnection({ ...config, database: undefined });
+    this.connection.connect();
+    const queries = [
+      `CREATE DATABASE IF NOT EXISTS ${config.database};`,
+      `USE ${config.database};`,
+      'CREATE TABLE IF NOT EXISTS counter (token CHAR(36) UNIQUE, count INT);',
+    ];
+    queries.forEach((query) => this.connection.query(query));
+  }
+
+  public async getCount(token: string): Promise<number> {
+    const query = `SELECT count FROM counter WHERE token='${token}';`;
+
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, (error, results) => {
+        if (error) return reject(error);
+
+        let result = parseInt(results[0].count, 10);
+        result = Number.isNaN(result) ? 0 : result;
+        resolve(result);
+      });
+    });
+  }
+
+  public incrementCount(token: string): void {
+    this.connection.query(`UPDATE counter SET count = count + 1 WHERE token='${token}';`);
+  }
+
+  private async updateCount(token: string, value: number) {
+    this.connection.query(`UPDATE counter SET count=${value} WHERE token='${token}';`);
+  }
+
+  public clearCount(token: string): void {
+    this.updateCount(token, 0);
+  }
+
+  public firstCount(token: string): void {
+    this.connection.query(`INSERT IGNORE INTO counter VALUES('${token}', 0);`);
+  }
+
+  public closeConnection(): void {
+    this.connection.end();
+  }
+}
---
+++ back-end/secao-08-programacao-orientada-a-objetos-poo-solid/dia-05-solid-principios-l-e-i/src/index.ts
@@ -0,0 +1,56 @@
+import Connector from './Connector';
+
+const token = 'ce42033d-9133-457a-a1a1-41ac0b63a333';
+const conn = new Connector({
+  host: 'mysqldb',
+  port: 3306,
+  database: 'counter',
+  user: 'root',
+  password: 'example',
+});
+
+const main = (connector: Connector) => {
+  connector.firstCount(token);
+
+  const logCount = async () => {
+    try {
+      const count = await connector.getCount(token);
+      console.log(count);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const doSomeIncrements = () => {
+    logCount();
+    connector.incrementCount(token);
+    connector.incrementCount(token);
+    connector.incrementCount(token);
+  };
+
+  const incrementsInterval = setInterval(doSomeIncrements, 500);
+
+  setTimeout(() => {
+    clearInterval(incrementsInterval);
+    logCount();
+    connector.clearCount(token);
+    connector.closeConnection();
+  }, 5500);
+};
+
+main(conn);
+
+/*
+Saída:
+0
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+*/
