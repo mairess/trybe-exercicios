87b13477b4387665f5b5ead6e79f582f89e586ae
mairess <trabalho.maires@gmail.com>
2024-03-26 11:34:00 -0300

    feat: automatized test

---
+++ ciencias-da-computacao/secacao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-em-testes/tests/codigo.py
@@ -0,0 +1,3 @@
+def is_odd(number):
+    "Retorna True se um número é ímpar, senão False."
+    return number % 2 != 0
---
+++ ciencias-da-computacao/secacao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-em-testes/tests/test_codigo.py
@@ -0,0 +1,11 @@
+from codigo import is_odd
+
+
+def test_is_odd_when_number_is_odd_returns_true():
+    "Para um número ímpar a função deve retornar o valor True"
+    assert is_odd(3) is True
+
+
+def test_is_odd_when_number_is_even_returns_false():
+    "Para um número par a função deve retornar o valor False"
+    assert is_odd(2) is False
