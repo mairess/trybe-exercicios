e9a1057f551b9a483f69e1b20c9800b8d13508d1
mairess <trabalho.maires@gmail.com>
2024-03-26 20:13:27 -0300

    test: test function fizzbuzz

---
+++ ciencias-da-computacao/secacao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-em-testes/exercise/exercise_5.py
@@ -0,0 +1,41 @@
+# 🚀 Exercício 5:
+# Realize esse exercício utilizando TDD: implemente os testes e depois a função 🧪
+
+# Escreva um programa que retorne uma lista com os valores numéricos de 1 a N, mas com as seguintes exceções:
+
+# Números divisíveis por 3 deve aparecer como “Fizz” ao invés do número;
+
+# Números divisíveis por 5 devem aparecer como “Buzz” ao invés do número;
+
+# Números divisíveis por 3 e 5 devem aparecer como “FizzBuzz” ao invés do número.
+
+# Exemplo: 3 -> [1, 2, "Fizz"].
+
+def fizzbuzz(n):
+    numbers = []
+    for number in range(1, n + 1):
+        if number % 15 == 0:
+            numbers.append("FizzBuzz")
+        elif number % 3 == 0:
+            numbers.append("Fizz")
+        elif number % 5 == 0:
+            numbers.append("Buzz")
+        else:
+            numbers.append(number)
+    return numbers
+
+
+def test_fizzbuzz_should_return_list_of_numbers():
+    assert fizzbuzz(2) == [1, 2]
+
+
+def test_fizzbuzz_divisible_by_three_should_be_fizz():
+    assert fizzbuzz(3)[-1] == "Fizz"
+
+
+def test_fizzbuzz_divisible_by_five_should_be_buzz():
+    assert fizzbuzz(5)[-1] == "Buzz"
+
+
+def test_fizzbuzz_divisible_by_five_and_three_should_be_fizzbuzz():
+    assert fizzbuzz(15)[-1] == "FizzBuzz"
