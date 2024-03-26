b618510a337b44b0f4c9e651fc05f3c45bfb9b76
mairess <trabalho.maires@gmail.com>
2024-03-26 10:52:44 -0300

    feat: filter grass pokemons

---
+++ ciencias-da-computacao/secacao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-em-testes/json_manipulation.py
@@ -0,0 +1,28 @@
+import json
+
+
+# with open("pokemons.json") as pokemons_file:
+#     content = pokemons_file.read()
+#     pokemons = json.loads(content)["results"]
+
+# print(pokemons[0])
+
+
+with open("pokemons.json") as pokemons_file:
+    pokemons = json.load(pokemons_file)["results"]
+
+grass_type_pokemons = []
+for pokemon in pokemons:
+    if "Grass" in pokemon["type"]:
+        grass_type_pokemons.append(pokemon)
+
+# with open("grass_type_pokemons.json", "w") as file:
+#     json_to_write = json.dumps(
+#         grass_type_pokemons
+#     )
+#     file.write(json_to_write)
+
+with open("grass_type_pokemons.json", "w") as file:
+    json.dump(grass_type_pokemons, file)
+
+print(pokemons[0])
