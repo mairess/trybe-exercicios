5cfb19278aafc797658c1a823c406377d9c0877f
mairess <trabalho.maires@gmail.com>
2024-04-08 11:27:09 -0300

    fix: add verification before split array

--- ciencias-da-computacao/secacao-03-estrutura-de-dados-I-listas-lineares/dia-01-arrays/exercise/exercise_02.py
+++ ciencias-da-computacao/secacao-03-estrutura-de-dados-I-listas-lineares/dia-01-arrays/exercise/exercise_02.py
@@ -7,13 +7,16 @@ cartas_01 = [2, 6, 4, 5]
 # resultado = [2, 4, 6, 5]
 
 # Exemplo 2:
-cartas_02 = [1, 4, 4, 7, 6, 6]
+cartas_02 = [1, 4, 4, 7, 6, 6, 9, 3, 5, 6]
 # cartas por parte = 3
 
 # resultado = [1, 7, 4, 6, 4, 6]
 
 
 def shuffle_cards(cards: list[int], card_per_part) -> list[int]:
+    if len(cards) % card_per_part != 0:
+        card_per_part = len(cards) // 2
+
     part_one = cards[:card_per_part]
     part_two = cards[card_per_part:]
 
@@ -24,6 +27,8 @@ def shuffle_cards(cards: list[int], card_per_part) -> list[int]:
         shuffled.append(part_two[index])
 
     return shuffled
+
+
 #  complexity is O(n)
 
 
