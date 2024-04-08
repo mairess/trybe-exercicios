72687f23603faf81d27e83e7b3565f3f863c8afe
mairess <trabalho.maires@gmail.com>
2024-04-08 11:37:12 -0300

    feat: add proposal solution

--- ciencias-da-computacao/secacao-03-estrutura-de-dados-I-listas-lineares/dia-01-arrays/exercise/exercise_02.py
+++ ciencias-da-computacao/secacao-03-estrutura-de-dados-I-listas-lineares/dia-01-arrays/exercise/exercise_02.py
@@ -29,6 +29,14 @@ def shuffle_cards(cards: list[int], card_per_part) -> list[int]:
     return shuffled
 
 
+# def shuffle(items):
+#     answer = []
+#     div_cards_by_two = len(items) // 2
+#     for offset in range(div_cards_by_two):
+#         answer.extend(items[offset::div_cards_by_two])
+#     return answer
+
+
 #  complexity is O(n)
 
 
