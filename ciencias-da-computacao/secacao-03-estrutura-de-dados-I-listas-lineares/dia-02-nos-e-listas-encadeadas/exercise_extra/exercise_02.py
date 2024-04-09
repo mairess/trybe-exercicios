8b460ca26b4ee5c74ca8d2dd41a9723ccb5f7bd2
mairess <trabalho.maires@gmail.com>
2024-04-09 09:06:52 -0300

    refactor: add proposal solution for exrcise two

--- ciencias-da-computacao/secacao-03-estrutura-de-dados-I-listas-lineares/dia-02-nos-e-listas-encadeadas/exercise_extra/exercise_02.py
+++ ciencias-da-computacao/secacao-03-estrutura-de-dados-I-listas-lineares/dia-02-nos-e-listas-encadeadas/exercise_extra/exercise_02.py
@@ -10,18 +10,42 @@
 # saída: 1 <-> 2
 
 
-def deleteDuplicates(head):
-    dummy = head(0)
-    dummy.next = head
-    prev = dummy
+# def deleteDuplicates(head):
+#     dummy = head(0)
+#     dummy.next = head
+#     prev = dummy
 
-    while head and head.next:
-        if head.val == head.next.val:
-            while head and head.next and head.val == head.next.val:
-                head = head.next
-            head = head.next
-            prev.next = head
-        else:
-            prev = prev.next
-            head = head.next
-    return dummy.next
+#     while head and head.next:
+#         if head.val == head.next.val:
+#             while head and head.next and head.val == head.next.val:
+#                 head = head.next
+#             head = head.next
+#             prev.next = head
+#         else:
+#             prev = prev.next
+#             head = head.next
+#     return dummy.next
+
+
+class DoublyLinkedList:
+    # ...
+    def index_of(self, value):
+        position = 1
+        current_value = self.head.next
+        while current_value != self.tail:
+            if current_value.value == value:
+                return position
+            current_value = current_value.next
+            position += 1
+        return -1
+
+
+def delete_duplicates(linked_list):
+    list_with_unique_elements = DoublyLinkedList()
+
+    while linked_list:
+        current_node = linked_list.remove_first()
+        if linked_list.index_of(current_node.value) == -1:
+            list_with_unique_elements.insert_last(current_node.value)
+
+    return list_with_unique_elements
