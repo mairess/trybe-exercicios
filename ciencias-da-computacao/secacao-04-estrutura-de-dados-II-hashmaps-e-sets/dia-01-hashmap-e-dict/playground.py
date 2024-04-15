309d6b96513b87478c9b77375c5f3ce1476e7ae8
mairess <trabalho.maires@gmail.com>
2024-04-15 09:56:44 -0300

    feat: add separate_words function

--- ciencias-da-computacao/secacao-04-estrutura-de-dados-II-hashmaps-e-sets/dia-01-hashmap-e-dict/playground.py
+++ ciencias-da-computacao/secacao-04-estrutura-de-dados-II-hashmaps-e-sets/dia-01-hashmap-e-dict/playground.py
@@ -1,4 +1,4 @@
-def get_most_frequent(numbers: list[int]):
+def get_most_frequent(numbers: list[int]) -> int:
     count = {}
     most_frequent = numbers[0]
 
@@ -12,3 +12,26 @@ def get_most_frequent(numbers: list[int]):
             most_frequent = count[num]
 
     return most_frequent
+
+
+# text = ["Ana", "ama", "Joao", "que", "ama", "Jose", "mais", "Jose", "nao", "ama", "Ana"]
+
+# resposta:
+# a: ["Ana", "ama", "ama", "Ana"]
+# j: ["Joao", "Jose", "Jose"]
+# q: ["que"]
+# m: ["mas"]
+# n: ["nao"]
+
+
+def separate_words(text: list[str]):
+    screen = {}
+
+    for word in text:
+        first_char = word[0]
+        if first_char not in screen:
+            screen[first_char] = [word]
+        else:
+            screen[first_char].append(word)
+
+    return screen
