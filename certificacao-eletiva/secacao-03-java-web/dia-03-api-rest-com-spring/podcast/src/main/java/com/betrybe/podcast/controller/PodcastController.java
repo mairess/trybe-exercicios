5b81b803c541941f33fd942ff13091cbe9098647
mairess <trabalho.maires@gmail.com>
2024-05-14 05:13:06 -0300

    feat: add GET routes /id and /search

--- certificacao-eletiva/secacao-03-java-web/dia-03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
+++ certificacao-eletiva/secacao-03-java-web/dia-03-api-rest-com-spring/podcast/src/main/java/com/betrybe/podcast/controller/PodcastController.java
@@ -1,16 +1,34 @@
 package com.betrybe.podcast.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/podcasts")
 public class PodcastController {
 
+//  @RequestMapping(method = RequestMethod.GET)
+//  public String getRoot() {
+//    return "Opa!!!!!!!!!1";
+//  }
+
   @GetMapping
   public String getRoot() {
-    return "Opa podcasts!";
+    return "Opa!!!!!!!!!1";
+  }
+
+  @GetMapping("/{id}")
+  public String getPodcast(@PathVariable Long id) {
+    return "You asked for Podcast with id: %d".formatted(id);
+  }
+
+  @GetMapping("/search")
+  public String searchPodcast(@RequestParam String title) {
+    return "You asked for Podcast with title: %s".formatted(title);
   }
 
 }
