dc1233a62bd2267315886fed3fafc7aa45354c08
mairess <trabalho.maires@gmail.com>
2024-05-17 05:24:08 -0300

    test: add @DisplayName annotation

--- certificacao-eletiva/secacao-03-java-web/dia-06-testes-com-junit/Calculadora/src/test/java/CalculadoraTest.java
+++ certificacao-eletiva/secacao-03-java-web/dia-06-testes-com-junit/Calculadora/src/test/java/CalculadoraTest.java
@@ -1,18 +1,27 @@
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Teste da classe Calculadora")
 public class CalculadoraTest {
 
   @Test
+  @DisplayName("Teste do método somar")
   public void testeSomar() {
+
     Calculadora calculadora = new Calculadora();
-    Assertions.assertEquals(34, calculadora.somar(32, 2));
+    assertEquals(34, calculadora.somar(32, 2));
+
   }
 
   @Test
+  @DisplayName("Teste do método subtrair")
   public void testeSubtrair() {
+
     Calculadora calculadora = new Calculadora();
-    Assertions.assertEquals(1, calculadora.subtrair(3, 2));
+    assertEquals(1, calculadora.subtrair(3, 2));
+
   }
+
 }
