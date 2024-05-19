e3545d3e353f20551b7c5cb58dff6a034580ce0a
mairess <trabalho.maires@gmail.com>
2024-05-19 16:06:18 -0300

    feat: add docker docker file

---
+++ certificacao-eletiva/secacao-03-java-web/dia-07-containers-java-e-docker/docker-spring/src/main/java/com/betrybe/dockerspring/DockerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.betrybe.dockerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSpringApplication.class, args);
+	}
+
+}
---
+++ certificacao-eletiva/secacao-03-java-web/dia-07-containers-java-e-docker/docker-spring/src/test/java/com/betrybe/dockerspring/DockerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betrybe.dockerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
