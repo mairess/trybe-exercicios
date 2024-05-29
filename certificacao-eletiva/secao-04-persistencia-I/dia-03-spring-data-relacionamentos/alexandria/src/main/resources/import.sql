29bf4d79f13732fadea6dd48348c3313ce25ee00
mairess <trabalho.maires@gmail.com>
2024-05-29 10:42:22 -0300

    feat: add seeders

---
+++ certificacao-eletiva/secao-04-persistencia-I/dia-03-spring-data-relacionamentos/alexandria/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- Query: SELECT * FROM alexandriadb.books
+-- exported data
+INSERT INTO `books` (`id`,`genre`,`title`,`publisher_id`) VALUES (1,"History","The Fall of the Roman Republic",NULL);
+INSERT INTO `books` (`id`,`genre`,`title`,`publisher_id`) VALUES (2,"History","The Civil War",NULL);
+INSERT INTO `books` (`id`,`genre`,`title`,`publisher_id`) VALUES (3,"History","The Rise and Fall of Alexandria: Birthplace of the Modern Mind",NULL);
